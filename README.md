0d92bc9a1dd485bd0c2e60bbf73fd4147e9d6332
westdoorblowcola <510908220@qq.com>
2016-04-14 13:51:23 +0800

    提交代码

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # django-chart
 提供一个简单生成图表的工具，主要是想直接复制粘贴就能生成相关的图
+
+# 运行
+- ```pip install -r requirements.txt```
+- ```python manage.py runserver```
+- 打开浏览器```http://127.0.0.1:8000/```,如图![](http://7xs8s1.com1.z0.glb.clouddn.com/django-chart-index.png)
+输入数据，点击```生成```按钮，如图![](http://7xs8s1.com1.z0.glb.clouddn.com/django-chart-chart.png)
+
+
+# 感谢
+[chartkick.py](https://github.com/mher/chartkick.py)
---
+++ chart/views.py
@@ -0,0 +1,36 @@
+# encoding:utf-8
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def string_to_int_or_float(value):
+    try:
+        value = int(value.strip())
+    except Exception:
+        value = float(value.strip())
+    return value
+
+
+def get_render_data(chart_type, chart_data):
+    lines = chart_data.split("\n")
+
+    result = [] if chart_type == "1" else {}
+    for line in lines:
+        line = line.strip()
+        if not line:
+            continue
+        first, second = line.split(",")
+        if chart_type == "1":
+            result.append([first.strip(), string_to_int_or_float(second)])
+        else:
+            result[first.strip()] = string_to_int_or_float(second)
+    return result
+
+
+def chart(request):
+    chart_type = request.POST['type']
+    render_data = get_render_data(chart_type, request.POST['chartdata'])
+    return render(request, 'charts.html' if chart_type == "1" else "charts-time.html", locals())
