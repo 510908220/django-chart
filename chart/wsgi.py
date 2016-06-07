12159841ca022aa563914b1f509cebcb1af0c0e9
westdoorblowcola <510908220@qq.com>
2016-06-07 21:23:14 +0800

    repair pylint error

--- chart/wsgi.py
+++ chart/wsgi.py
@@ -7,6 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
+# pylint: disable=C0103
+
 import os
 
 from django.core.wsgi import get_wsgi_application
